ef37e9dc0f7d4f16eaa3b4cb318031ae91d025ba
David Mihai <david.gmihai@gmail.com>
2015-09-20 18:14:26 -0700

    Testing push

--- coldwar/src/coldwar/Game.java
+++ coldwar/src/coldwar/Game.java
@@ -4,6 +4,7 @@ public class Game {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		System.out.println("GLORIOUS SOVIET PROGRAM WILL VANQUISH CAPITALISM");
 	}
 
 }
