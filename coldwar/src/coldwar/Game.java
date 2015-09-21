e7d0e06e3978601eaf120725facb90ab91916df9
machinule <machinule@users.noreply.github.com>
2015-09-20 20:46:55 -0700

    initial game state builder

--- coldwar/src/coldwar/Game.java
+++ coldwar/src/coldwar/Game.java
@@ -1,12 +1,17 @@
 package coldwar;
 
+import coldwar.GameStateOuterClass.GameState;
+import com.google.protobuf.TextFormat;
+
 public class Game {
 
 	static Renderer renderer;
 	
 	public static void main(String[] args) {
+		GameState initialGameState = GameState.newBuilder().setTurn(0).build();
 		renderer = new Renderer();
 		renderer.TestUpdate("Hello, Capitalist World!");
+		System.out.println(TextFormat.printToString(initialGameState));
 	}
 
 }
