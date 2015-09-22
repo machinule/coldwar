0b074aef5d7451b761c06d8fa797887eb66ea738
machinule <machinule@users.noreply.github.com>
2015-09-21 21:43:23 -0700

    basic networking

--- coldwar/src/coldwar/Game.java
+++ coldwar/src/coldwar/Game.java
@@ -1,17 +1,55 @@
 package coldwar;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+
 import coldwar.GameStateOuterClass.GameState;
 import com.google.protobuf.TextFormat;
+
+// Specific libGDX backend implementation imports.
+import com.badlogic.gdx.backends.lwjgl.*;
+
+final class Pipe extends Thread {
+	public Pipe(InputStream in, OutputStream out) {
+		super(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					int b = 0;
+					while (b != -1) {
+						b = in.read();
+						out.write(b);
+					}					
+				} catch (IOException ex) {
+					System.out.println("Oops");
+					return;
+				}
+			}
+		});
+		start();
+	}
+}
 
 public class Game {
 
 	static Renderer renderer;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		GameState initialGameState = GameState.newBuilder().setTurn(0).build();
 		renderer = new Renderer();
 		renderer.TestUpdate("Hello, Capitalist World!");
 		System.out.println(TextFormat.printToString(initialGameState));
+		Peer peer = new Peer(new LwjglNet());
+		if (args[0].equals("connect")) {
+			peer.Connect(args[1], Integer.valueOf(args[2]));
+		} else if (args[0].equals("host")) {
+			peer.Host(Integer.valueOf(args[1]));
+		}
+		Thread in = new Pipe(System.in, peer.getOutputStream());
+		Thread out = new Pipe(peer.getInputStream(), System.out);
+		in.join();
+		out.join();
 	}
 
 }
--- coldwar/src/coldwar/Peer.java
+++ coldwar/src/coldwar/Peer.java
@@ -1,5 +1,50 @@
 package coldwar;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.badlogic.gdx.Net;
+import com.badlogic.gdx.net.*;
+
 public class Peer {
 
+	private Net.Protocol protocol;
+	private Socket socket;  // TODO(hesswill): dispose.
+	private Net net;
+	
+	public Peer(Net networkImpl) {
+		protocol = Net.Protocol.TCP;
+		net = networkImpl;
+	}
+
+	public synchronized void Host(int port) {
+		if (socket != null) {
+			return;
+		}
+		ServerSocketHints hints = new ServerSocketHints();
+		hints.acceptTimeout = 0;
+		socket = net.newServerSocket(protocol, port, hints).accept(new SocketHints());
+	}
+	
+	public synchronized void Connect(String host, int port) {
+		if (socket != null) {
+			return;
+		}
+		// TODO(machinule): timeouts, error handling.
+		socket = net.newClientSocket(protocol, host, port, new SocketHints());
+	}
+	
+	public InputStream getInputStream() {
+		if (socket == null) {
+			return null;
+		}
+		return socket.getInputStream();
+	}
+	
+	public OutputStream getOutputStream() {
+		if (socket == null) {
+			return null;
+		}
+		return socket.getOutputStream();
+	}
 }
