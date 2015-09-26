270af227a7664bd1e2f98486efc06d7d40aea89c
David Mihai <david.gmihai@gmail.com>
2015-09-26 15:20:58 -0700

    Rebase continue

--- coldwar/src/coldwar/Client.java
+++ coldwar/src/coldwar/Client.java
@@ -1,5 +1,8 @@
 package coldwar;
 
 public class Client {
-
+	
+	//public void Update(GameState state, MoveList local, MoveList remote) { 
+		
+	//}
 }
---
+++ coldwar/src/coldwar/Logger.java
@@ -0,0 +1,52 @@
+package coldwar;
+
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+
+public class Logger {
+	
+	static PrintWriter writer; 
+	static boolean Console = true;
+	static boolean File = true;
+
+	public static void Start() {
+		Start(true, true);
+	}
+	
+	public static void Start(boolean in_file, boolean in_console) {
+		try {
+			writer = new PrintWriter("log.txt");
+		} catch(Exception e) {
+			System.out.println("LOGGER EXCEPTION: " + e.toString());
+		}
+	}
+	
+	public static void Close() {
+		writer.flush();
+		writer.close();
+	}
+	
+	public static void Dbg(String in) {
+		Log(TmStmp() + ": DEBUG: " + in);
+	}
+	public static void Info(String in) {
+		Log(TmStmp() + ": INFO: " + in);
+	}
+	public static void Warn(String in) {
+		Log(TmStmp() + ": WARN: " + in);
+	}
+	public static void Err(String in) {
+		Log(TmStmp() + ": ERROR: " + in);
+	}
+	
+	private static void Log(String log) {
+		writer.println(log);
+		writer.flush();
+	}
+	
+	private static String TmStmp() {
+		java.util.Date date= new java.util.Date();
+		Timestamp tmpstmp = new Timestamp(date.getTime());
+		return tmpstmp.toString();
+	}
+}
