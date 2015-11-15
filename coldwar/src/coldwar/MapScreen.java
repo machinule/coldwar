e9aba12b18cc4a933d87ad7d6c59c7afde4cda90
machinule <machinule@users.noreply.github.com>
2015-11-15 15:47:11 -0800

    Add province info cards to the map screen.

--- coldwar/src/coldwar/MapScreen.java
+++ coldwar/src/coldwar/MapScreen.java
@@ -1,10 +1,31 @@
 package coldwar;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+import coldwar.ProvinceOuterClass.Province;
+
 public class MapScreen extends AbstractScreen {
 		
     public MapScreen(final ColdWarGame game) {
         super(game);
     }
     
-    
+	@Override
+	public void show() {
+		super.show();
+		TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("textures/uiskin.atlas"));
+		Skin skin = new Skin(Gdx.files.internal("textures/uiskin.json"), atlas);
+        Table table = new Table(skin);
+        table.setFillParent(true);
+        table.setDebug(true);
+        stage.addActor(table);
+        MoveBuilder moveBuilder = new MoveBuilder();
+        for (Province.Id id : Province.Id.values()) {
+            table.add(new ProvinceInfoCard(Province.newBuilder().setId(id).build(), moveBuilder, skin));
+            table.row();        	
+        }
+	}    
 }
--- coldwar/src/coldwar/ProvinceInfoCard.java
+++ coldwar/src/coldwar/ProvinceInfoCard.java
@@ -1,7 +1,7 @@
 package coldwar;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -11,39 +11,36 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import coldwar.ProvinceOuterClass.Province;
 
 
-public class ProvinceInfoCard extends Group {
+public class ProvinceInfoCard extends Table {
 
 	protected Province province;
-	protected Table table;
 	protected MoveBuilder moveBuilder;
 	protected Skin skin;
 	
 	public ProvinceInfoCard(Province province, MoveBuilder moveBuilder, Skin skin) {
+		super();
 		this.province = province;
 		this.moveBuilder = moveBuilder;
 		this.skin = skin;
-		this.table = new Table();
-		this.table.setFillParent(true);
-		this.table.setDebug(true);
-		addActor(this.table);
+		this.setDebug(true);
 		
-		this.table.add(new Label(province.getId().getValueDescriptor().getName(), this.skin));
-		this.table.row();
+		this.add(new Label(province.getId().getValueDescriptor().getName(), this.skin));
+		this.row();
         TextButton reduceButton = new TextButton("-", skin);
         reduceButton.addListener(new ChangeListener() {
             public void changed (ChangeEvent event, Actor actor) {
             	Logger.Info("\"Reduce\" button pressed on " + province.getId().getValueDescriptor().getName());
             }
         });
-        this.table.add(reduceButton);
-		this.table.add(new Label("Influence: 0", this.skin));
+        this.add(reduceButton);
+		this.add(new Label("Influence: 0", this.skin));
         TextButton increaseButton = new TextButton("+", skin);
         increaseButton.addListener(new ChangeListener() {
             public void changed (ChangeEvent event, Actor actor) {
             	Logger.Info("\"Increase\" button pressed on " + province.getId().getValueDescriptor().getName());
             }
         });
-        this.table.add(increaseButton);
+        this.add(increaseButton);
 	}
 	
 	
