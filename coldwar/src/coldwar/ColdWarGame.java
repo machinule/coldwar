609027d46012ec16097dd03fa20a04b3b6bbb8f1
machinule <machinule@users.noreply.github.com>
2015-11-21 16:45:20 -0800

    comments, cleanup, and a bit of refactoring.

--- coldwar/src/coldwar/ColdWarGame.java
+++ coldwar/src/coldwar/ColdWarGame.java
@@ -6,41 +6,79 @@ import coldwar.MoveListOuterClass.MoveList;
 import coldwar.logic.MoveBuilder;
 import coldwar.ui.SplashScreen;
 
+/**
+ * ColdWarGame coordinates the application life cycle. It should not contain
+ * game logic.
+ * 
+ * @see com.badlogix.gdx.Game
+ */
 public class ColdWarGame extends Game {
 
-	public static final String LOG = ColdWarGame.class.getSimpleName();
-	public Peer peer;
+	private final Peer peer;
 
 	public ColdWarGame(final Peer peer) {
 		super();
 		this.peer = peer;
 	}
 
-	public void connect(final String host, final int port) {
-		this.peer.Connect(host, port);
-	}
-
+	/**
+	 * Called when the application is created. Immediately enters the splash
+	 * screen.
+	 * 
+	 * @see com.badlogic.gdx.ApplicationListener#create()
+	 */
 	@Override
 	public void create() {
 		this.setScreen(new SplashScreen(this));
 	}
 
+	/**
+	 * Called when the application is rendered.
+	 * 
+	 * @see com.badlogic.gdx.Game#render()
+	 */
 	@Override
-	public void dispose() {
+	public void render() {
+		super.render();
 	}
 
+	/**
+	 * Called when the application is disposed.
+	 * 
+	 * @see com.badlogic.gdx.Game#dispose()
+	 */
+	@Override
+	public void dispose() {
+		// TODO: clean up application elements.
+	}
+
+	/**
+	 * End the current turn.
+	 * 
+	 * @param moveBuilder
+	 */
 	public void endTurn(final MoveBuilder moveBuilder) {
 		final MoveList local = moveBuilder.getMoveList();
 		this.peer.sendMoveList(local);
 	}
 
+	/**
+	 * Host a game.
+	 * 
+	 * @param parseInt
+	 */
 	public void host(final int parseInt) {
 		this.peer.Host(parseInt);
 	}
 
-	@Override
-	public void render() {
-		super.render(); // important!
+	/**
+	 * Connect to a remote game.
+	 * 
+	 * @param host
+	 * @param port
+	 */
+	public void connect(final String host, final int port) {
+		this.peer.Connect(host, port);
 	}
 
 }
