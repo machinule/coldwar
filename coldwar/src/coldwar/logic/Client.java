82384e34ce53afb8738dddc069e3e30a743024c3
David Mihai <david.gmihai@gmail.com>
2015-11-23 20:45:09 -0800

    Populated initial test map

--- coldwar/src/coldwar/logic/Client.java
+++ coldwar/src/coldwar/logic/Client.java
@@ -24,9 +24,7 @@ public abstract class Client {
 				.setVersion("0.0.1")
 				.setHeat(Settings.getConstInt("Starting Heat"))
 				.setTurn(0);
-		for (final Province.Id id : Province.Id.values()) {
-			state.addProvincesBuilder().setId(id).setInfluence(0).setDissidents(false);
-		}
+		populateProvinces(state);
 		return state;
 	}
 	
@@ -39,6 +37,143 @@ public abstract class Client {
 		ComputationCache cache = new ComputationCache(this.state, this.getUSAMove(), this.getUSSRMove());
 		this.state = Computations.getNextGameState(cache);
 		Logger.Info("Next game state: " + this.state.toString());
+	}
+	
+	protected void populateProvinces(GameState.Builder state) {
+		
+		// CENTRAL AMERICA
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.MEXICO)
+			.setStability(3)
+			.addAdjacency(Province.Id.GUATEMALA)
+			.addAdjacency(Province.Id.HONDURAS);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.GUATEMALA)
+			.addAdjacency(Province.Id.MEXICO)
+			.addAdjacency(Province.Id.HONDURAS)
+			.addAdjacency(Province.Id.NICARAGUA);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.HONDURAS)
+			.addAdjacency(Province.Id.MEXICO)
+			.addAdjacency(Province.Id.GUATEMALA)
+			.addAdjacency(Province.Id.NICARAGUA);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.NICARAGUA)
+			.addAdjacency(Province.Id.CUBA)
+			.addAdjacency(Province.Id.HONDURAS)
+			.addAdjacency(Province.Id.GUATEMALA)
+			.addAdjacency(Province.Id.COSTA_RICA);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.COSTA_RICA)
+			.setStability(2)
+			.setGov(Province.Government.DEMOCRACY)
+			.addAdjacency(Province.Id.NICARAGUA)
+			.addAdjacency(Province.Id.PANAMA);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.PANAMA)
+			.setInfluence(2)
+			.setBase(Province.Id.US)
+			.addAdjacency(Province.Id.NICARAGUA)
+			.addAdjacency(Province.Id.ECUADOR);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.CUBA)
+			.setInfluence(2)
+			.setDissidents(true)
+			.addAdjacency(Province.Id.US)
+			.addAdjacency(Province.Id.NICARAGUA)
+			.addAdjacency(Province.Id.MEXICO)
+			.addAdjacency(Province.Id.HAITI);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.HAITI)
+			.addAdjacency(Province.Id.CUBA)
+			.addAdjacency(Province.Id.DOMINICAN_REP);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.DOMINICAN_REP)
+			.setGov(Province.Government.AUTOCRACY)
+			.addAdjacency(Province.Id.HAITI)
+			.addAdjacency(Province.Id.LESS_ANTILLES);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.LESS_ANTILLES)
+			.addAdjacency(Province.Id.DOMINICAN_REP)
+			.addAdjacency(Province.Id.VENEZUELA)
+			.addAdjacency(Province.Id.GUYANA);
+		
+		// SOUTH AMERICA
+		
+		state.addProvincesBuilder()
+		.setId(Province.Id.COLUMBIA)
+			.addAdjacency(Province.Id.DOMINICAN_REP)
+			.addAdjacency(Province.Id.VENEZUELA)
+			.addAdjacency(Province.Id.GUYANA);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.ECUADOR)
+			.setStability(2)
+			.addAdjacency(Province.Id.COLUMBIA)
+			.addAdjacency(Province.Id.PERU);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.PERU)
+			.setStability(2)
+			.setGov(Province.Government.AUTOCRACY)
+			.addAdjacency(Province.Id.ECUADOR)
+			.addAdjacency(Province.Id.BOLIVIA)
+			.addAdjacency(Province.Id.CHILE);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.CHILE)
+			.setStability(2)
+			.setGov(Province.Government.DEMOCRACY)
+			.addAdjacency(Province.Id.PERU)
+			.addAdjacency(Province.Id.ARGENTINA)
+			.addAdjacency(Province.Id.BOLIVIA);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.BOLIVIA)
+			.setStability(2)
+			.addAdjacency(Province.Id.PERU)
+			.addAdjacency(Province.Id.ARGENTINA)
+			.addAdjacency(Province.Id.CHILE);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.ARGENTINA)
+			.setStability(2)
+			.addAdjacency(Province.Id.BOLIVIA)
+			.addAdjacency(Province.Id.BRAZIL)
+			.addAdjacency(Province.Id.CHILE);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.BRAZIL)
+			.setStability(2)
+			.addAdjacency(Province.Id.GUYANA)
+			.addAdjacency(Province.Id.VENEZUELA)
+			.addAdjacency(Province.Id.ARGENTINA);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.GUYANA)
+			.setGov(Province.Government.COLONY)
+			.setOccupier(Province.Id.GREAT_BRITAIN)
+			.addAdjacency(Province.Id.VENEZUELA)
+			.addAdjacency(Province.Id.BRAZIL)
+			.addAdjacency(Province.Id.LESS_ANTILLES);
+		
+		state.addProvincesBuilder()
+			.setId(Province.Id.VENEZUELA)
+			.setStability(2)
+			.addAdjacency(Province.Id.COLUMBIA)
+			.addAdjacency(Province.Id.BRAZIL)
+			.addAdjacency(Province.Id.GUYANA)
+			.addAdjacency(Province.Id.LESS_ANTILLES);
 	}
 
 	public abstract void endTurn();
