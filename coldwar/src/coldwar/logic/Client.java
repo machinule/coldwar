950edb64a5f16d2d8be459e11bdcee9a61a3e44f
David Mihai <david.gmihai@gmail.com>
2015-11-28 18:26:52 -0800

    Political pressure mechanics

--- coldwar/src/coldwar/logic/Client.java
+++ coldwar/src/coldwar/logic/Client.java
@@ -81,7 +81,8 @@ public abstract class Client {
 			.setLabel("Mexico")
 			.setStabilityBase(2)
 			.addAdjacency(Province.Id.GUATEMALA)
-			.addAdjacency(Province.Id.HONDURAS);
+			.addAdjacency(Province.Id.HONDURAS)
+			.addAdjacency(Province.Id.CUBA);
 		
 		settings.addProvincesBuilder()
 			.setId(Province.Id.GUATEMALA)
