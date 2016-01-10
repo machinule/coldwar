8ccc300d91efcf11be3708be7ce3f67041b1a071
machinule <machinule@users.noreply.github.com>
2016-01-10 12:55:34 -0800

    initial gradle import

---
+++ desktop/src/com/berserkbentobox/coldwar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.berserkbentobox.coldwar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.berserkbentobox.coldwar.ColdWarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ColdWarGame(), config);
+	}
+}
