279f3f49394588876b64aa55f6fd8971dd2f243c
machinule <machinule@users.noreply.github.com>
2016-01-23 14:22:45 -0800

    Initial fixing of tech screen UI

--- core/src/com/berserkbentobox/coldwar/ui/TechCard.java
+++ core/src/com/berserkbentobox/coldwar/ui/TechCard.java
@@ -5,7 +5,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
-import com.berserkbentobox.coldwar.Logger;
+import com.berserkbentobox.coldwar.Settings;
+import com.berserkbentobox.coldwar.Technology.TechnologySettings;
 import com.berserkbentobox.coldwar.logic.Client;
 
 public class TechCard extends Table {
@@ -16,16 +17,15 @@ public class TechCard extends Table {
 	protected Button infoBox;
 	
 	protected String id;
-//	protected TechSettings techSettings;
+	protected TechnologySettings settings;
 	
 	public TechCard(final Client client,
-					final String id,
+					final TechnologySettings t,
 					final Skin skin) {
 		super();
 		this.client = client;
 		this.skin = skin;
-		this.id = id;
-//		this.techSettings = client.getMoveBuilder().getComputedGameState().getTechSettings(id);
+		this.settings = t;
 		
 		infoBox = createLayout();
 		this.add(infoBox).size(200, 50);
@@ -33,32 +33,25 @@ public class TechCard extends Table {
 	
 	protected Button createLayout() {
 		Button ret = new Button(this.skin);
-		//ret.setDebug(Settings.isDebug());
-//		Logger.Dbg("Looking for " + id + ": " + client.getMoveBuilder().getComputedGameState().getTechSettings(id));
-//		DynamicLabel progressions = new DynamicLabel(client,
-//				c -> "" + techSettings.getProgressions(),
-//				skin);
-//		DynamicLabel progress = new DynamicLabel(client,
-//				c -> "" + c.getMoveBuilder().getComputedGameState().getTech(client.getPlayer(), id).getProgress(),
-//				skin);
-//		DynamicLabel name = new DynamicLabel(client,
-//				c -> "" + techSettings.getLabel(),
-//				skin);
-//		
-//		if(client.getMoveBuilder().getComputedGameState().isTechAvailable(client.getPlayer(), id)) {
-//			ret.setColor(Color.LIGHT_GRAY);
-//		} else if(!client.getMoveBuilder().getComputedGameState().isTechCompleted(client.getPlayer(), id)) {
-//			ret.setColor(Color.DARK_GRAY);
-//		}
+		ret.setDebug(Settings.isDebug());
+		DynamicLabel progressions = new DynamicLabel(client,
+				c -> "" + this.settings.getNumProgressions(),
+				skin);
+		DynamicLabel progress = new DynamicLabel(client,
+				c -> "" + 0,
+				skin);
+		DynamicLabel name = new DynamicLabel(client,
+				c -> "" + this.settings.getLabel(),
+				skin);
 		
-//		name.setAlignment(1); //Center in cell
-//		name.setFontScale((float)0.75);
-//		progressions.setFontScale((float)1.5);
-//		progress.setFontScale((float)1.5);
+		name.setAlignment(1); //Center in cell
+		name.setFontScale((float)0.75);
+		progressions.setFontScale((float)1.5);
+		progress.setFontScale((float)1.5);
 		
-//		ret.add(progressions).left().padTop(5).expand();
-//		ret.add(name).center();
-//		ret.add(progress).right().padTop(5).expand();
+		ret.add(progressions).left().padTop(5).expand();
+		ret.add(name).center();
+		ret.add(progress).right().padTop(5).expand();
 		ret.row();
 		ret.add();
 		
